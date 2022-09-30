a9224fdb3e4261b39a19905f5c523eadf47e1665
Reverson Thaiam <reverson.santos@frotabank.com.br>
2022-09-29 23:24:05 -0300

    first commit

---
+++ build/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, name: 'anuncio 1' }
+    ]);
+});
+app.listen(3333);
